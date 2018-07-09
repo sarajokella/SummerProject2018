16d646c291abcf4f0308efd7dcd4d4874d20fa22
Sara Jokela <sjokela@ppepc187.physics.gla.ac.uk>
2018-07-09 14:30:10 +0100

    edited README and job

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # SummerProject2018
 
-Use the Espresso Performance Monitor to study tagging performance in bins
+Use the Espresso Performance Monitor to study tagging performance in bins 
 of some variable.
+
+This package enables the EPM to be run over bins of equal bin statistics.
+The EPM is run twice, once to produce the calibration parameters for the 
+individual taggers and combine the OS taggers using the even/odd events,
+and a second time to calibrate the taggers and combine the OS and SS taggers
+using the odd/even events. Directories labelled Calib_Bin_#_even/odd
+and Comb_Bin_#_odd/even are created with the results from each EPM run.
+
+histbins2.py defines a function called binboundaries which inputs a histogram
+and a desired number of bins and return the bin boundaries for bins of equal
+statistics.
+
+EPMoptsTemplate.py is the template for the option file used in the EPM runs.
+replacement.py contains two functions; dict which defines the replacements 
+for the template, and process which applies the replacements and writes them
+to an option file.
+
+job.py combines the functions from above to run the EPM in the specified 
+directories for the bins within the bin boundaries. Therefore, to run this
+package only the job.py script must be modifed by:
+
+1. Choosing the desired root file and histogram 
+2. Define the changes to the template. It should be noted that the default
+setting is chosen such that the EPM runs twice as stated above
+3. Run job.py
--- job.py
+++ job.py
@@ -14,28 +14,29 @@ tree.Draw("nTracks>>h1")
 from replacement import process, dict 
 word = ["even", "odd", "even"]
 selection = ["&&eventNumber%2==0", "&&eventNumber%2!=0", "&&eventNumber%2==0"]
+user_input = raw_input("Enter subdirectory name: ")
+g = int(user_input)
                        
-for n in range(0,5):      
+for n in range(0,g):      
         for t in range(0,2):
-                #user_input = raw_input("Enter subdirectory name: ")
                 x = "Calib_Bin_"+ str(n) + "_" + word[t]
                 y = "Comb_Bin_" + str(n) + "_" + word[t+1]
         
                 if not os.path.exists(x):
                         os.makedirs(x) #make directories
                         os.makedirs(y)
-                        bins = binboundaries(h1,5) #determine bin boundaries
+                        bins = binboundaries(h1,g) #determine bin boundaries
                         j = """ " """
                         b = j[1]+ str(bins[n]) +"<=nTracks&&nTracks<" + str(bins[n+1]) + selection[t] + j[1]
                 
-                        dic = dict("50000", b, "", "#", "test")         
+                        dic = dict("-1", b, "", "#", "test")         
                         process("EPMoptsTemplate.py", "result.py", dic) #create option file
                         shutil.move("result.py", os.path.join(x)) #move option file into correct directory
                         os.chdir(x)
                         os.system(r"../../builddir/bin/SimpleEvaluator result.py 2>&1 | tee out.log") #calibration
                         os.chdir("..")
                         c = j[1] + str(bins[n]) +"<=nTracks&&nTracks<" + str(bins[n+1]) + selection[t+1] + j[1]
-                        dic2 = dict("50000", c, "#", "", x)
+                        dic2 = dict("-1", c, "#", "", x)
                         process("EPMoptsTemplate.py", "result2.py", dic2)
                         shutil.move("result2.py", os.path.join(y))
                         os.chdir(y)
