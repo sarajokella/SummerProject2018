db5c4ab3a9f39548e987d73a17bbaf93c261e48f
Sara Jokela <sjokela@ppepc187.physics.gla.ac.uk>
2018-07-09 15:47:42 +0100

    Makes graphs of even and odd events

--- FinalGraphs.py
+++ FinalGraphs.py
@@ -7,8 +7,10 @@ from out2 import values
 from histbins2 import binboundaries
 import math
        
-tag = [0]*5
-yerrors = [0]*5
+tago = [0]*5
+tage = [0]*5
+yerrorse = [0]*5
+yerrorso = [0]*5
 xerrors = [0]*5
 bins = [0]*5
 
@@ -17,43 +19,58 @@ f = file.Get("Bu2JpsiKplusDetached;1.root")
 tree = f.Get("DecayTree;2")
 tree.Draw("nTracks>>h1")
 
+
 for n in range(0,5):
-        bins[n] = (binboundaries(h1,5)[n+1] + binboundaries(h1,5)[n])/2
-        x = "Comb_Bin" + str(n)
-        y = "Comb_Bin" + str(n+1)
-        shutil.move("out2.py", os.path.join(x))
+        bins[n] = (binboundaries(h1,5)[n+1] + binboundaries(h1,5)[n])/2 #find halfway point between bin boundaries
+        x = "Comb_Bin_" + str(n) + "_even"
+        y = "Comb_Bin_" + str(n) + "_odd"
+        shutil.move("out2.py", os.path.join(x)) #out2.py is the script that extracts the tagging power information
         os.chdir(x) 
-        tag[n] = values("out.log")[7]
-        yerrors[n] = math.sqrt(values("out.log")[8]**2 + values("out.log")[9]**2)
-        xerrors[n] = (binboundaries(h1,5)[n+1] - binboundaries(h1,5)[n])/2
+        tage[n] = values("out.log")[7] #tagging power for even directories
+        yerrorse[n] = math.sqrt(values("out.log")[8]**2 + values("out.log")[9]**2) #extracts y errors
+        xerrors[n] = (binboundaries(h1,5)[n+1] - binboundaries(h1,5)[n])/2 # finds x errors
+        shutil.move("out2.py", os.path.join("..",y))
+        os.chdir(os.path.join("..",y))
+        tago[n] = values("out.log")[7] #tagging power for odd directories
+        yerrorso[n] = math.sqrt(values("out.log")[8]**2 + values("out.log")[9]**2)
         shutil.move("out2.py", "..")
         os.chdir("..")
 
-c = TCanvas('c1', 'Final Graph', 200, 10, 1500, 1000)
+c = TCanvas('c1', 'Final Graph', 200, 10, 1800, 1000)
+c.Divide(2,1)
 c.cd()
 
-cbins = ROOT.std.vector("double")()
-ctag = ROOT.std.vector("double")()
-cyerrors = ROOT.std.vector("double")()
+cbins = ROOT.std.vector("double")() #converts python list into vectors that Root can process
+ctage = ROOT.std.vector("double")()
+ctago = ROOT.std.vector("double")()
+cyerrorse = ROOT.std.vector("double")()
+cyerrorso = ROOT.std.vector("double")()
 cxerrors = ROOT.std.vector("double")()
 
 for i in bins:
         cbins.push_back(i)
-for i in tag:
-        ctag.push_back(i)
-for i in yerrors:
-        cyerrors.push_back(i)
+for i in tago:
+        ctago.push_back(i)
+for i in tage:
+        ctage.push_back(i)
+for i in yerrorse:
+        cyerrorse.push_back(i)
+for i in yerrorso:
+        cyerrorso.push_back(i)
 for i in xerrors:
         cxerrors.push_back(i)
 
-g2 = TGraphErrors(5, cbins.data(), ctag.data(), cxerrors.data(), cyerrors.data())
+g2 = TGraphErrors(5, cbins.data(), ctage.data(), cxerrors.data(), cyerrorse.data())
+g3 = TGraphErrors(5, cbins.data(), ctago.data(), cxerrors.data(), cyerrorso.data()) 
 
-#g1 = TGraphErrors(5)
-#for i in range(1,6):
-       # g1.SetPoint(i, bins[i-1], tag[i-1])
-
-c.cd()
+c.cd(1)
+g2.SetTitle("Even Events")
 g2.GetXaxis().SetTitle("N Tracks")
 g2.GetYaxis().SetTitle("Effective Tagging Power (%)")
 g2.Draw("AP")
+c.cd(2)
+g3.SetTitle("Odd Events")
+g3.GetXaxis().SetTitle("N Tracks")
+g3.GetYaxis().SetTitle("Effective Tagging Power (%)")
+g3.Draw("AP")
 c.Print("FinalGraph.png")
