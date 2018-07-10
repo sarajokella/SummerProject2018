ec66a4a4cfa15323bf460ae041bfd6af298ef93b
Sara Jokela <sjokela@ppepc187.physics.gla.ac.uk>
2018-07-10 15:35:43 +0100

    made file easier to modify by external user

--- job.py
+++ job.py
@@ -5,17 +5,27 @@ import sys
 import shutil
 from ROOT import *
 from histbins2 import binboundaries
+from replacement import process, dict 
 
+###############Variables to change##################################################
+
+g = int(5) #NUMBER OF DESIRED BINS
+variable = "nTracks" #DESIRED VARIABLE TO ANALYSE
+number = "-1" #NUMBER OF EVENTS TO RUN OVER, DEFAULT "-1" IS ALL EVENTS
+path = "../../builddir/" #PATH TO THE EPM
+
+#ROOT FILE
 file = TFile.Open("/data/lhcb04/mschiller/SummerProject2018/Bu2JpsiKplus_MC_Down_Upgrade_OptSummer2017.root")
 f = file.Get("Bu2JpsiKplusDetached;1.root")
 tree = f.Get("DecayTree;2")
-tree.Draw("nTracks>>h1")
+#tree.Draw("nTracks>>h1")
+tree.Draw(variable + ">>h1")
 
-from replacement import process, dict 
+####################################################################################
+
 word = ["even", "odd", "even"]
 selection = ["&&eventNumber%2==0", "&&eventNumber%2!=0", "&&eventNumber%2==0"]
-user_input = raw_input("Enter number of bin boundaries: ")
-g = int(user_input)
+paths = path + "bin/SimpleEvaluator result.py 2>&1 | tee out.log" 
                        
 for n in range(0,g):      
         for t in range(0,2):
@@ -27,22 +37,22 @@ for n in range(0,g):
                         os.makedirs(y)
                         bins = binboundaries(h1,g) #determine bin boundaries
                         j = """ " """
-                        b = j[1]+ str(bins[n]) +"<=nTracks&&nTracks<" + str(bins[n+1]) + selection[t] + j[1]
+                        b = j[1]+ str(bins[n]) + "<=" + variable + "&&" + variable + "<" + str(bins[n+1]) + selection[t] + j[1]
                 
-                        dic = dict("-1", b, "", "#", "test")         
+                        dic = dict(number, b, "", "#", "test")         
                         process("EPMoptsTemplate.py", "result.py", dic) #create option file
                         shutil.move("result.py", os.path.join(x)) #move option file into correct directory
                         os.chdir(x)
                         #First Run of the EPM
-                        os.system(r"../../builddir/bin/SimpleEvaluator result.py 2>&1 | tee out.log") #calibration
+                        os.system(paths) #calibration
                         os.chdir("..")
-                        c = j[1] + str(bins[n]) +"<=nTracks&&nTracks<" + str(bins[n+1]) + selection[t+1] + j[1]
-                        dic2 = dict("-1", c, "#", "", x)
-                        process("EPMoptsTemplate.py", "result2.py", dic2)
-                        shutil.move("result2.py", os.path.join(y))
+                        c = j[1] + str(bins[n]) +"<=" + variable + "&&" + variable + "<" + str(bins[n+1]) + selection[t+1] + j[1]
+                        dic2 = dict(number, c, "#", "", x)
+                        process("EPMoptsTemplate.py", "result.py", dic2)
+                        shutil.move("result.py", os.path.join(y))
                         os.chdir(y)
                         #Second Run of the EPM
-                        os.system(r"../../builddir/bin/SimpleEvaluator result2.py 2>&1 | tee out.log") #combination
+                        os.system(path) #combination
                         os.chdir("../")
                 else:  
                         print "subdirectory already exists!"
